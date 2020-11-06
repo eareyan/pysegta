78debeb45514963cc22740f0b960d04be7685216
eareyan <enrique3@gmail.com>
2020-11-06 12:03:54 -0500

    poker experiments, w.e., regret

--- non_uniform.py
+++ non_uniform.py
@@ -1,36 +1,117 @@
 import math
-import pprint
-import pandas as pd
-import numpy as np
-import poker_game
-import scipy.stats
 
 
-def psp(active_set, target_epsilon, sampling_schedule, failure_probability_schedule, c, sample_condition, compute_utility):
+def compute_confidence(m, U, V, size_of_game, c, current_delta):
+    """
+    :param m:
+    :param U:
+    :param V:
+    :param size_of_game:
+    :param c:
+    :param current_delta:
+    """
+    v_hat = (V - ((U * U) / m)) / (m - 1)
+    epsilon_v = (c * c * math.log(3 * size_of_game / current_delta)) / (
+        m - 1
+    ) + math.sqrt(
+        math.pow((c * c * math.log(3 * size_of_game / current_delta)) / (m - 1), 2)
+        + ((2 * c * c * v_hat * math.log(3 * size_of_game / current_delta)) / (m - 1))
+    )
+    return min(
+        c * math.sqrt((math.log(3 * size_of_game / current_delta)) / (2 * m)),
+        ((c * math.log(3 * size_of_game / current_delta)) / (3 * m))
+        + math.sqrt(
+            ((2 * (v_hat + epsilon_v)) * math.log(3 * size_of_game / current_delta)) / m
+        ),
+    )
+
+
+def prune_by_regret(un_dominated_set, statistics, confidence, neighborhood):
+
+    # To start with, the set of pruning pairs is empty.
+    prune_by_regret_set = set()
+
+    # Only un_dominated strategies are candidates for regret pruning.
+    for p, (s1, s2) in un_dominated_set:
+
+        # Compute the maximum deviation, up to estimation errors, in the corresponding neighborhood.
+        best_alternative = max(
+            [
+                (
+                    statistics[p, (x, s2) if p == 1 else (s1, x)][1]
+                    / statistics[p, (x, s2) if p == 1 else (s1, x)][0]
+                )
+                - confidence[p, (x, s2) if p == 1 else (s1, x)]
+                for x in filter(
+                    lambda x: (p, (x, s2))
+                    if p == 1
+                    else (p, (s1, x)) in un_dominated_set,
+                    neighborhood[p, (s1, s2)],
+                )
+            ],
+            default=-math.inf,
+        )
+
+        # Prune the (player, strategy profile) pair in case the regret, up to errors, is too big.
+        avg = statistics[p, (s1, s2)][1] / statistics[p, (s1, s2)][0]
+        if avg + confidence[p, (s1, s2)] < best_alternative:
+            prune_by_regret_set.add((p, (s1, s2)))
+
+    return prune_by_regret_set
+
+
+def psp(
+    neighborhood,
+    active_set,
+    target_epsilon,
+    sampling_schedule,
+    fail_prob_schedule,
+    c,
+    sample_condition,
+    compute_utility,
+    allow_regret_prune=True,
+    allow_well_est_prune=True,
+):
     """
     Progressive sampling with pruning.
+    :param neighborhood:
     :param active_set:
     :param target_epsilon:
     :param sampling_schedule:
-    :param failure_probability_schedule:
+    :param fail_prob_schedule:
     :param c:
     :param sample_condition:
     :param compute_utility:
+    :param allow_regret_prune:
+    :param allow_well_est_prune:
     :return:
     """
     # Assume the size of the game is the size of the first active set, which is supposed to be the entire game.
     size_of_game = len(active_set)
 
     # Initialize estimates.
-    statistics = {(player, strategy_profile): (0, 0, 0) for player, strategy_profile in active_set}
-    confidence = {(player, strategy_profile): math.inf for player, strategy_profile in active_set}
+    statistics = {
+        (player, strategy_profile): (0, 0, 0) for player, strategy_profile in active_set
+    }
+    confidence = {
+        (player, strategy_profile): math.inf for player, strategy_profile in active_set
+    }
 
     # Collect results.
-    results = {}
-    len_active_set = [0 for _ in range(0, len(sampling_schedule))]
+    size_active_set = [-1 for _ in range(0, len(sampling_schedule))]
+    size_inactive_set = [-1 for _ in range(0, len(sampling_schedule))]
+    size_well_estimated_set = [-1 for _ in range(0, len(sampling_schedule))]
+    size_dominated_set = [-1 for _ in range(0, len(sampling_schedule))]
+
+    # Keep track of categories.
+    well_estimated_set = set()
+    un_dominated_set = active_set.copy()
+    dominated_set = set()
 
     # Iterate through the sampling schedule.
-    for t, (current_number_of_samples, current_delta) in enumerate(zip(sampling_schedule, failure_probability_schedule)):
+    for t, (current_number_of_samples, current_delta) in enumerate(
+        zip(sampling_schedule, fail_prob_schedule)
+    ):
 
         # Print progress. Debug only.
         # print(f"len(active_set) = {len(active_set)}")
@@ -40,30 +121,79 @@ def psp(active_set, target_epsilon, sampling_schedule, failure_probability_sched
 
         # For each active (player, strategy profile) pair, refine estimates.
         for player, strategy_profile in active_set:
-            current_estimates = compute_utility(player=player, strategy_profile=strategy_profile, condition=current_condition)
+
+            # Compute utilities of pair (player, strategy_profile) only in case the pair is not well estimated and not dominated.
+            current_estimates = compute_utility(
+                player=player,
+                strategy_profile=strategy_profile,
+                condition=current_condition,
+            )
 
             # Add estimate to current counts.
-            statistics[player, strategy_profile] = [sum(x) for x in zip(statistics[player, strategy_profile], current_estimates)]
+            statistics[player, strategy_profile] = [
+                sum(x)
+                for x in zip(statistics[player, strategy_profile], current_estimates)
+            ]
 
             # m is the total number of samples for this (player, strategy profile), U its total sum, and V its squared sum.
             m, U, V = statistics[player, strategy_profile]
 
-            # Math stuff.
-            v_hat = (V - ((U * U) / m)) / (m - 1)
-            epsilon_v = (c * c * math.log(3 * size_of_game / current_delta)) / (m - 1) + \
-                        math.sqrt(math.pow((c * c * math.log(3 * size_of_game / current_delta)) / (m - 1), 2) + ((2 * c * c * v_hat * math.log(3 * size_of_game / current_delta)) / (m - 1)))
-            confidence[player, strategy_profile] = min(c * math.sqrt((math.log(3 * size_of_game / current_delta)) / (2 * m)),
-                                                       ((c * math.log(3 * size_of_game / current_delta)) / (3 * m))
-                                                       + math.sqrt(((2 * (v_hat + epsilon_v)) * math.log(3 * size_of_game / current_delta)) / m))
+            # Compute the confidence of the (player, strategy_profile) pair.
+            confidence[player, strategy_profile] = compute_confidence(
+                m, U, V, size_of_game, c, current_delta
+            )
+
             # Debug print, output what the estimates look like so far.
             # print(f"{player, strategy_profile} \t {m} \t {statistics[player, strategy_profile]} \t\t\t {confidence[player, strategy_profile]:.6f}")
 
-        active_set = set(filter(lambda pair: confidence[pair] > target_epsilon, active_set))
-        len_active_set[t] = len(active_set)
+        # Prune by regret
+        if allow_regret_prune:
+            prune_by_regret_set = prune_by_regret(
+                un_dominated_set=un_dominated_set,
+                statistics=statistics,
+                confidence=confidence,
+                neighborhood=neighborhood,
+            )
+
+            # Update the dominated/un_dominated set.
+            dominated_set = dominated_set.union(prune_by_regret_set)
+            un_dominated_set = un_dominated_set - dominated_set
+            assert len(un_dominated_set) + len(dominated_set) == size_of_game
+            size_dominated_set[t] = len(dominated_set)
+
+        # Update the different sets.
+        if allow_well_est_prune:
+            well_estimated_set = well_estimated_set.union(
+                set(filter(lambda pair: confidence[pair] <= target_epsilon, active_set))
+            )
+            size_well_estimated_set[t] = len(well_estimated_set)
+            inactive_set = well_estimated_set.union(dominated_set)
+        else:
+            inactive_set = dominated_set
+
+        active_set = (active_set - well_estimated_set) - dominated_set
+
+        assert len(active_set) + len(inactive_set) == size_of_game
+        size_active_set[t] = len(active_set)
+        size_inactive_set[t] = len(inactive_set)
+
+        # Debug prints, to manually inspect the progress of the algorithm.
+        # print(f"active_set \t\t\t= {len(active_set)}")
+        # print(f"inactive_set \t\t\t= {len(inactive_set)}")
+        # print(f"well_estimated_set \t= {len(well_estimated_set)}")
+        # print(f"dominated_set \t\t= {len(dominated_set)}")
+        # print(f"un_dominated_set \t= {len(un_dominated_set)}")
+        # print(f"well or dominated \t= {len(well_estimated_set.union(dominated_set))}")
 
         if len(active_set) == 0:
-            # No more active pairs, break and finish the algorithm. 
+            # No more active pairs, break and finish the algorithm.
             break
 
-    results['len_active_set'] = len_active_set
+    # Collect and return and results.
+    results = {
+        "size_active_set": size_active_set,
+        "size_inactive_set": size_inactive_set,
+        "size_well_estimated_set": size_well_estimated_set,
+        "size_dominated_set": size_dominated_set,
+    }
     return statistics, confidence, results
--- poker_experiments.py
+++ poker_experiments.py
@@ -1,14 +1,20 @@
-import json
+import itertools as it
 import math
-import pprint
-import names
 import os
 import time
+
+import names
 import pandas as pd
+from prettytable import PrettyTable
 
 from non_uniform import psp
-from poker_game import draw_game, compute_game_stats, poker_dealer, poker_player_utility
-from prettytable import PrettyTable
+from poker_game import (
+    draw_game,
+    compute_game_stats,
+    poker_dealer,
+    poker_player_utility,
+    compute_neighbors,
+)
 
 
 def compute_n(c, delta, epsilon, size_of_game):
@@ -16,88 +22,80 @@ def compute_n(c, delta, epsilon, size_of_game):
 
 
 def compute_eps(c, delta, size_of_game, number_of_samples):
-    return c * math.sqrt((math.log((2.0 * size_of_game) / delta)) / (2.0 * number_of_samples))
+    return c * math.sqrt(
+        (math.log((2.0 * size_of_game) / delta)) / (2.0 * number_of_samples)
+    )
 
 
 def create_ptable(dictionary):
     ptable = PrettyTable()
-    ptable.title = 'Running experiments with parameters'
-    ptable.field_names = ['Parameter', 'Value']
+    ptable.title = "Running experiments with parameters"
+    ptable.field_names = ["Parameter", "Value"]
     for k, v in dictionary.items():
         ptable.add_row([k, v])
     return ptable
 
 
-def sample_hands():
-    number_samples_mean = 10000
-    sample = []
-    for _ in range(0, 5000):
-        p1_hand, p2_hand, _ = get_initial_game(size_hand=4)
-        strat_prof = (p1_hand, p2_hand)
+# Exogenous Parameters.
+exogenous_parameters = {
+    "experiment_name": names.get_full_name().replace(" ", "_"),
+    "size_of_game": 50,
+    "target_epsilon": 0.15,
+    "c": 2,
+    "delta": 0.1,
+    # 'number_of_games': 100,
+    "number_of_games": 1,
+    "number_of_psp_runs_per_game": 30,
+    # "number_of_psp_runs_per_game": 10,
+}
 
-        expectation_p1 = expected_utility(1, strat_prof)
-        expectation_p2 = expected_utility(2, strat_prof)
+# Endogenous Parameters.
+endogenous_parameters = {
+    "m": compute_n(
+        c=exogenous_parameters["c"],
+        delta=exogenous_parameters["delta"],
+        epsilon=exogenous_parameters["target_epsilon"],
+        size_of_game=exogenous_parameters["size_of_game"],
+    ),
+    "fail_prob_schedule": [exogenous_parameters["delta"] / 4 for _ in range(0, 4)],
+}
+endogenous_parameters["sampling_schedule"] = [
+    math.ceil(endogenous_parameters["m"] / 4),
+    math.ceil(endogenous_parameters["m"] / 2),
+    math.ceil(endogenous_parameters["m"]),
+    math.ceil(2 * endogenous_parameters["m"]),
+]
 
-        m, U, V = poker_player_utility(1, strat_prof, poker_dealer(number_samples_mean))
-        empirical_mean_p1 = U / m
-
-        m, U, V = poker_player_utility(2, strat_prof, poker_dealer(number_samples_mean))
-        empirical_mean_p2 = U / m
-
-        variance_p1 = variance_utility(1, strat_prof)
-        variance_p2 = variance_utility(2, strat_prof)
-
-        print(strat_prof)
-        print(f"{expectation_p1 : .4f} \t {empirical_mean_p1: .4f} \t {variance_p1 : .4f}")
-        print(f"{expectation_p2 : .4f} \t {empirical_mean_p2 : .4f} \t {variance_p2 : .4f}")
-        sample += [[p1_hand, p2_hand, expectation_p1, expectation_p2, empirical_mean_p1, empirical_mean_p2, variance_p1, variance_p2]]
-
-    pd.DataFrame(sample, columns=['hand_p1', 'hand_p2', 'E[p1]', 'E[p2]', 'emp_p1', 'emp_p2', 'VAR[p1]', 'VAR[p2]']).to_csv('hands_stats.csv', index=False)
-
-
-if __name__ == '__main__':
-
-    # Exogenous Parameters.
-    exogenous_parameters = {'experiment_name': names.get_full_name().replace(' ', '_'),
-                            'size_of_game': 50,
-                            'target_epsilon': 0.15,
-                            'c': 2,
-                            'delta': 0.1,
-                            'number_of_games': 100,
-                            'number_of_psp_runs_per_game': 30}
+if __name__ == "__main__":
 
     # Check if an experiment with this name already exists so as not to overwrite any previous results.
-    if os.path.exists(f"poker_experiments_data/experiment_{exogenous_parameters['experiment_name']}"):
-        print(f"Experiment {exogenous_parameters['experiment_name']} already exists, try again. ")
+    if os.path.exists(
+        f"poker_experiments_data/experiment_{exogenous_parameters['experiment_name']}"
+    ):
+        print(
+            f"Experiment {exogenous_parameters['experiment_name']} already exists, try again. "
+        )
         exit()
-
-    # Endogenous Parameters.
-    endogenous_parameters = {}
-    endogenous_parameters['m'] = compute_n(c=exogenous_parameters['c'],
-                                           delta=exogenous_parameters['delta'],
-                                           epsilon=exogenous_parameters['target_epsilon'],
-                                           size_of_game=exogenous_parameters['size_of_game'])
-    endogenous_parameters['failure_probability_schedule'] = [exogenous_parameters['delta'] / 4 for _ in range(0, 4)]
-    endogenous_parameters['sampling_schedule'] = [math.ceil(endogenous_parameters['m'] / 4),
-                                                  math.ceil(endogenous_parameters['m'] / 2),
-                                                  math.ceil(endogenous_parameters['m']),
-                                                  math.ceil(2 * endogenous_parameters['m'])]
 
     # Print parameters
     parameter_ptable = create_ptable({**exogenous_parameters, **endogenous_parameters})
     print(parameter_ptable)
-    with open(f"poker_experiments_data/experiment_{exogenous_parameters['experiment_name']}.txt", 'w') as the_file:
+    with open(
+        f"poker_experiments_data/experiment_{exogenous_parameters['experiment_name']}.txt",
+        "w",
+    ) as the_file:
         the_file.write(str(parameter_ptable))
 
     # Run experiments
     list_of_games = []
     list_of_psp_results = []
-    for game in range(0, exogenous_parameters['number_of_games']):
+    for game in range(0, exogenous_parameters["number_of_games"]):
         # Time how long it takes to produce results for a single game
         t0_game = time.time()
 
         # Draw a game
         p1_poker_hand, p2_poker_hand, the_active_set = draw_game()
+        neighborhood = compute_neighbors(p1_poker_hand, p2_poker_hand, the_active_set)
 
         # Compute the game's statistics
         max_variance, sum_variance, _ = compute_game_stats(the_active_set)
@@ -107,26 +105,72 @@ if __name__ == '__main__':
         print(f"\tmax_variance = {max_variance} \t sum_variance = {sum_variance}")
         print(f"\tp1_poker_hand = {p1_poker_hand} \t p2_poker_hand = {p2_poker_hand}")
 
-        list_of_games += [[game, p1_poker_hand, p2_poker_hand, max_variance, sum_variance]]
+        list_of_games += [
+            [game, p1_poker_hand, p2_poker_hand, max_variance, sum_variance]
+        ]
+        for allow_regret_prune, allow_well_est_prune in it.product(
+            [False, True], [False, True]
+        ):
+            for run in range(0, exogenous_parameters["number_of_psp_runs_per_game"]):
+                # Run PSP
+                _, _, results = psp(
+                    neighborhood=neighborhood,
+                    active_set=the_active_set,
+                    target_epsilon=exogenous_parameters["target_epsilon"],
+                    sampling_schedule=endogenous_parameters["sampling_schedule"],
+                    fail_prob_schedule=endogenous_parameters["fail_prob_schedule"],
+                    c=exogenous_parameters["c"],
+                    sample_condition=poker_dealer,
+                    compute_utility=poker_player_utility,
+                    allow_regret_prune=allow_regret_prune,
+                    allow_well_est_prune=allow_well_est_prune,
+                )
 
-        for run in range(0, exogenous_parameters['number_of_psp_runs_per_game']):
-            # Run PSP
-            _, _, results = psp(active_set=the_active_set,
-                                target_epsilon=exogenous_parameters['target_epsilon'],
-                                sampling_schedule=endogenous_parameters['sampling_schedule'],
-                                failure_probability_schedule=endogenous_parameters['failure_probability_schedule'],
-                                c=exogenous_parameters['c'],
-                                sample_condition=poker_dealer,
-                                compute_utility=poker_player_utility)
-
-            # Print results for debugging purposes
-            # pprint.pprint(statistics)
-            # print(f"len_active_set = {results['len_active_set']}")
-            list_of_psp_results += [[game] + results['len_active_set']]
+                # Print results for debugging purposes
+                # pprint.pprint(statistics)
+                # print(f"size_active_set = {results['size_active_set']}")
+                list_of_psp_results += [
+                    [
+                        game,
+                        1 if allow_regret_prune else 0,
+                        1 if allow_well_est_prune else 0,
+                    ]
+                    + results["size_active_set"]
+                    + results["size_well_estimated_set"]
+                    + results["size_dominated_set"]
+                ]
         print(f"done!, took {time.time() - t0_game: .4f} sec")
 
     # Save games and results.
-    pd.DataFrame(list_of_games, columns=['game_id', 'p1_poker_hand', 'p2_poker_hand', 'max_variance', 'sum_variance']). \
-        to_csv(f"poker_experiments_data/experiment_{exogenous_parameters['experiment_name']}_games.csv", index=False)
-    pd.DataFrame(list_of_psp_results, columns=['game_id'] + [f"psp_iter_{i}" for i, _ in enumerate(endogenous_parameters['sampling_schedule'])]). \
-        to_csv(f"poker_experiments_data/experiment_{exogenous_parameters['experiment_name']}_psp.csv", index=False)
+    pd.DataFrame(
+        list_of_games,
+        columns=[
+            "game_id",
+            "p1_poker_hand",
+            "p2_poker_hand",
+            "max_variance",
+            "sum_variance",
+        ],
+    ).to_csv(
+        f"poker_experiments_data/experiment_{exogenous_parameters['experiment_name']}_games.csv",
+        index=False,
+    )
+    pd.DataFrame(
+        list_of_psp_results,
+        columns=["game_id", "allow_regret_prune", "allow_well_est_prune"]
+        + [
+            f"size_active_set{i}"
+            for i, _ in enumerate(endogenous_parameters["sampling_schedule"])
+        ]
+        + [
+            f"size_well_estimated_set{i}"
+            for i, _ in enumerate(endogenous_parameters["sampling_schedule"])
+        ]
+        + [
+            f"size_dominated_set{i}"
+            for i, _ in enumerate(endogenous_parameters["sampling_schedule"])
+        ],
+    ).to_csv(
+        f"poker_experiments_data/experiment_{exogenous_parameters['experiment_name']}_psp.csv",
+        index=False,
+    )
